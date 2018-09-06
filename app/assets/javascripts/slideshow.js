0635bb5f6f9694382840e77577e50aa868d747fd
Michael Wong <mail@michaelwongsilversmith.com>
2018-09-06 19:54:53 +0200

    add images to coding

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,17 +1,17 @@
-let slideIndex = 0;
-carousel();
+// let slideIndex = 0;
+// carousel();
 
-function carousel() {
-    let i;
-    let x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 4000); // Change image every 2 seconds
-}
+// function carousel() {
+//     let i;
+//     let x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1}
+//     x[slideIndex-1].style.display = "block";
+//     setTimeout(carousel, 4000); // Change image every 2 seconds
+// }
 
 
 
