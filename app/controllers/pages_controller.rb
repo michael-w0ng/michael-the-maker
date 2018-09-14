d610e72c52afc557240ac52ac6ffc3287b2e92e9
Michael Wong <mail@michaelwongsilversmith.com>
2018-09-14 16:04:49 +0200

    footer, Contact page, and formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,9 @@ class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
   end
+
+  def download_pdf
+    send_file "#{Rails.root}/public/docs/MW_CV_Sep_2018.pdf", type: "application/pdf", x_sendfile: true
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/pages/:page" => "pages#show"
+  get 'download_pdf', to: "pages#download_pdf"
+
 end
